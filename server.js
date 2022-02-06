a3cfb34cadd9e2456c58543d2d3df13beca0a64d
Dekguh <kadekteguhmahesa@gmail.com>
2022-02-06 22:09:25 +0800

    update

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.post('/buat-pembayaran', async (req, res) => {
  * callback when success make payment
  * docs https://tripay.co.id/developer?tab=callback
  */
-app.post('/callback-payment', async (req, res) => {
+app.post('/callback-payment', (req, res) => {
     var json = req.body
     var signature = crypto.createHmac("sha256", PAYMENT_PRIVATE_KEY)
         .update(json)
